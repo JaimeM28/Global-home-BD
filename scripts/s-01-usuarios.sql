1a893de05c6dae83e6ddc9672603a50d063d819b
JaimeM28 <mirandaserranojaimemanuel@gmail.com>
2024-04-27 19:43:05 -0600

    feat: script para creacion de usuarios

---
+++ scripts/s-01-usuarios.sql
@@ -0,0 +1,34 @@
+--@Autor(es): Jaime Manuel Miranda Serrano y Diego Adrian Del Razo Sanchez
+--@Fecha creación: 17/04/2024
+--@Descripción: Creación de usuarios y roles
+
+Prompt Iniciando con sys 
+connect sys/system as sysdba
+
+prompt Borrando usuario md_proy_admin
+drop user md_proy_admin cascade;
+
+prompt Borrando usuario md_proy_invitado
+drop user md_proy_invitado cascade;
+
+Prompt Borrando roles
+drop role rol_admin;
+drop role rol_invitado; 
+
+prompt creando usuario md_proy_admin
+create user md_proy_admin identified by admin quota unlimited on users;
+
+Prompt creando al usuario md_proy_invitado
+create user md_proy_invitado identified by invitado quota 0 on users;
+
+Prompt creando roles
+create role rol_admin;
+grant create session, create table, create view, create sequence, create synonym to rol_admin;
+create role rol_invitado;
+grant create session to rol_invitado; 
+
+Prompt Asignando roles
+grant rol_admin to md_proy_admin;
+grant rol_invitado to md_proy_invitado;
+
+disconnect
--- scripts/test.sql
+++ scripts/test.sql
@@ -1,3 +1,4 @@
 --@Autor(es): Miranda Serrano Jaime Manuel
 --@Fecha creación: 27/04/2024
 --@Descripción: Archivo para ejecutar todos los scripts
+@s-01-usuarios.sql
