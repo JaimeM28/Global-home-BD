0e09fba257397b388944cac5a60d44e1621de9cf
Diego Del Razo <158770774+sorryihateyou@users.noreply.github.com>
2024-06-04 20:49:18 -0600

    Create s-05-secuencias-02.sql

---
+++ scripts/s-05-secuencias-02.sql
@@ -0,0 +1,107 @@
+--@Autor(es): Miranda Serrano Jaime Manuel y Del Razo Sánchez Diego Adrián
+--@Fecha creación: 4/06/2024
+--@Descripción: Creación de secuencias 
+
+Prompt creando secuencia usuario_seq
+create sequence usuario_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia vivienda_seq
+create sequence vivienda_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia status_vivienda_seq
+create sequence status_vivienda_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia historico_status_vivienda_seq
+create sequence historico_status_vivienda_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia tipo_servicio_seq
+create sequence tipo_servicio_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia vivienda_tipo_servicio_seq
+create sequence vivienda_tipo_servicio_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia mensaje_seq
+create sequence mensaje_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia vivienda_vacacion_espera_seq
+create sequence vivienda_vacacion_espera_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia alquiler_seq
+create sequence alquiler_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia alquiler_calificacion_seq
+create sequence alquiler_calificacion_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia contrato_renta_seq
+create sequence contrato_renta_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia clabe_seq
+create sequence clabe_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
+
+Prompt creando secuencia vivienda_renta_clabe_seq
+create sequence vivienda_renta_clabe_seq 
+start with 1
+increment by 1
+nomaxvalue
+nocycle
+cache 10;
