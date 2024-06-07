90617c576ef7b7179929f903c7e8cdc3829bfd16
Diego Del Razo <158770774+sorryihateyou@users.noreply.github.com>
2024-06-06 19:06:38 -0600

    Update s-11-tr-vivienda-vacacion-tr.sql minusculas

--- scripts/s-11-tr-vivienda-vacacion-tr.sql
+++ scripts/s-11-tr-vivienda-vacacion-tr.sql
@@ -2,30 +2,31 @@
 --@Fecha creación: 05/06/2024
 --@Descripción:  Creación de trigger para enviar mensajes cuando una vivienda de vacaciones esté disponible
 
-CREATE OR REPLACE TRIGGER vivienda_vacacion_disponible_trg
-AFTER INSERT OR UPDATE OF costo_dia, maximo_dias, costo_aparto
-ON vivienda_vacacion
-FOR EACH ROW
-DECLARE
-  v_usuario_id    usuario.usuario_id%TYPE;
-  v_mensaje_id    mensaje.mensaje_id%TYPE;
-  v_titulo        VARCHAR2(40) := 'Vivienda Vacacional Disponible';
-  v_cuerpo        VARCHAR2(500);
-BEGIN
-  -- Componer el cuerpo del mensaje
-  v_cuerpo := 'La vivienda con ID ' || :NEW.vivienda_id || ' está ahora disponible para alquiler vacacional.';
+create or replace trigger vivienda_vacacion_disponible_trg
+after insert or update of costo_dia, maximo_dias, costo_aparto
+on vivienda_vacacion
+for each row
+declare
+  v_usuario_id    usuario.usuario_id%type;
+  v_mensaje_id    mensaje.mensaje_id%type;
+  v_titulo        varchar2(40) := 'vivienda vacacional disponible';
+  v_cuerpo        varchar2(500);
+begin
+  -- componer el cuerpo del mensaje
+  v_cuerpo := 'la vivienda con id ' || :new.vivienda_id || ' está ahora disponible para alquiler vacacional.';
 
-  -- Seleccionar todos los usuarios
-  FOR r_usuario IN (SELECT usuario_id FROM usuario) LOOP
-    -- Obtener el ID del mensaje de la secuencia
-    SELECT mensaje_seq.NEXTVAL INTO v_mensaje_id FROM dual;
+  -- seleccionar todos los usuarios
+  for r_usuario in (select usuario_id from usuario) loop
+    -- obtener el id del mensaje de la secuencia
+    select mensaje_seq.nextval into v_mensaje_id from dual;
 
-    -- Insertar un mensaje para cada usuario
-    INSERT INTO mensaje (mensaje_id, titulo, cuerpo, usuario_id, vivienda_id)
-    VALUES (v_mensaje_id, v_titulo, v_cuerpo, r_usuario.usuario_id, :NEW.vivienda_id);
-  END LOOP;
-END;
+    -- insertar un mensaje para cada usuario
+    insert into mensaje (mensaje_id, titulo, cuerpo, usuario_id, vivienda_id)
+    values (v_mensaje_id, v_titulo, v_cuerpo, r_usuario.usuario_id, :new.vivienda_id);
+  end loop;
+end;
 /
 
--- Mensaje de éxito
-PROMPT Trigger creado correctamente para enviar mensajes a los usuarios cuando una vivienda vacacional esté disponible.
+-- mensaje de éxito
+prompt trigger creado correctamente para enviar mensajes a los usuarios cuando una vivienda vacacional esté disponible.
+
